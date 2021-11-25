43c36d80d03c12fabf1f7a1efb533a2d23385b08
andres herrera <aherrera004@outlook.com>
2021-11-25 12:32:12 -0800

    work on layout

--- Web/src/app/app.module.ts
+++ Web/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import {MatIconModule} from '@angular/material/icon';
     BrowserAnimationsModule,
     LoginModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatSidenavModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
